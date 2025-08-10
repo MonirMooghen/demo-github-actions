72c262e7639fbb5a39ca2fb2e0893350786ee3d5
MonirMooghen <momo0003@stud.kea.dk>
2025-08-10 19:53:18 +0200

    Initial commit

---
+++ src/test/java/kea/momo/demogithubactions/DemoGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.momo.demogithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
