36e817446d42fe6f0eb7a03f08d6075a61933387
MonirMooghen <momo0003@stud.kea.dk>
2025-08-10 19:58:58 +0200

    2. commit

--- src/test/java/kea/momo/demogithubactions/DemoGithubActionsApplicationTests.java
+++ src/test/java/kea/momo/demogithubactions/DemoGithubActionsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoGithubActionsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
-}
+} //
